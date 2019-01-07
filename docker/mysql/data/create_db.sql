3f4ddce7de29d0e184fe56b0a114bce26e6ded81
Yannis <yayouzer@gmail.com>
2019-01-07 20:51:36 +0100

    projets contenants 2 projets indépendants avec cucumber dans 1 et swagger+springboot dans l'autre

---
+++ docker/mysql/data/create_db.sql
@@ -0,0 +1,22 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Oct 29, 2018 at 03:17 PM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `AMT-DB`
+--
+
+-- DROP DATABASE `AMT-DB`;
+-- CREATE DATABASE `AMT-DB`;
+-- USE `AMT-DB`;
+
+-- --------------------------------------------------------
+
