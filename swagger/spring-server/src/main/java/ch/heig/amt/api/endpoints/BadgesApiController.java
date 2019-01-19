c4294077d76d6b6fa3b34546048cac9528953529
Cyril de Bourgues <cyrildebourgues@gmail.com>
2019-01-19 21:17:19 +0100

    Badge, Event & part of PointScale endpoints are implemented Database is fully operational

--- swagger/spring-server/src/main/java/ch/heig/amt/api/endpoints/BadgesApiController.java
+++ swagger/spring-server/src/main/java/ch/heig/amt/api/endpoints/BadgesApiController.java
@@ -2,30 +2,93 @@ package ch.heig.amt.api.endpoints;
 
 import ch.heig.amt.api.BadgesApi;
 import ch.heig.amt.api.model.BadgeDTO;
+import ch.heig.amt.entities.ApplicationEntity;
+import ch.heig.amt.entities.BadgeEntity;
+import ch.heig.amt.repositories.ApplicationRepository;
+import ch.heig.amt.repositories.BadgeRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+
 import java.util.List;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-01-17T18:45:54.991+01:00")
 @Controller
 public class BadgesApiController implements BadgesApi {
 
+    private BadgeRepository badgeRepository;
+    private ApplicationRepository applicationRepository;
+
+    public BadgesApiController(BadgeRepository badgeRepository, ApplicationRepository applicationRepository) {
+        this.badgeRepository = badgeRepository;
+        this.applicationRepository = applicationRepository;
+    }
+
     @Override
     public ResponseEntity<Void> badgesDelete(String xGamificationToken, String name) {
 
+        // Check user is allowed to post event
+        ApplicationEntity applicationEntity = this.applicationRepository.findByToken(xGamificationToken);
+        if (applicationEntity == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
 
+        // Find matching badge & delete it
+        BadgeEntity badgeEntity = badgeRepository.findBadgeEntityByName(name);
+        badgeRepository.delete(badgeEntity);
 
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<List<BadgeDTO>> badgesGet(String xGamificationToken) {
-        return null;
+
+        // Check user is allowed to post event
+        ApplicationEntity applicationEntity = this.applicationRepository.findByToken(xGamificationToken);
+        if (applicationEntity == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        // Fetch list of badges
+        Iterable<BadgeEntity> badgeEntities = this.badgeRepository.findAll();
+        List<BadgeDTO> badgeDTOS = null;
+        for (BadgeEntity badgeEntity: badgeEntities) {
+            badgeDTOS.add(toBadgeDTO(badgeEntity));
+        }
+
+        return new ResponseEntity(badgeDTOS, HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<Void> badgesPost(String xGamificationToken, BadgeDTO body) {
-        return null;
+    public ResponseEntity<Void> badgesPost(String xGamificationToken, @RequestBody BadgeDTO body) {
+
+        // Check user is allowed to post event
+        ApplicationEntity applicationEntity = this.applicationRepository.findByToken(xGamificationToken);
+        if (applicationEntity == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        // save new badge to database
+        BadgeEntity badgeEntity = toBadgeEntity(body);
+        badgeRepository.save(badgeEntity);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    public BadgeEntity toBadgeEntity(BadgeDTO badgeDTO) {
+
+        BadgeEntity badgeEntity = new BadgeEntity();
+        badgeEntity.setName(badgeDTO.getName());
+
+        return badgeEntity;
+    }
+
+    public BadgeDTO toBadgeDTO(BadgeEntity badgeEntity) {
+
+        BadgeDTO badgeDTO = new BadgeDTO();
+        badgeDTO.setName(badgeEntity.getName());
+
+        return badgeDTO;
     }
 }
---
+++ swagger/spring-server/src/main/java/ch/heig/amt/repositories/PointScaleRepository.java
@@ -0,0 +1,11 @@
+package ch.heig.amt.repositories;
+
+import ch.heig.amt.entities.PointScaleEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PointScaleRepository extends CrudRepository<PointScaleEntity, Long> {
+
+    PointScaleEntity findPointScaleEntitiesByName(String name);
+}
